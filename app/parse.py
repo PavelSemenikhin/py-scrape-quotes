7d102844ccd4adb25d0802b8c8ca413f6d099914
Pavlo Semenikhin <pashock1337@gmail.com>
2025-10-19 20:20:54 +0300

    solution

--- app/parse.py
+++ app/parse.py
@@ -1,8 +1,7 @@
 import csv
 from dataclasses import dataclass
 import requests
-from bs4 import BeautifulSoup
-
+from bs4 import BeautifulSoup, Tag
 
 BASE_URL = "https://quotes.toscrape.com/"
 
@@ -14,7 +13,7 @@ class Quote:
     tags: list[str]
 
 
-def get_single_quote(quote):
+def parse_single_quote(quote: Tag) -> Quote:
     tags_list = [tag.text.strip() for tag in quote.select(".tags a.tag")]
     return Quote(
         text=quote.select_one(".text").text.strip(),
@@ -23,28 +22,26 @@ def get_single_quote(quote):
     )
 
 
-def get_quotes_from_page(page_soup):
+def get_quotes_from_page(page_soup: BeautifulSoup) -> list[Quote]:
     quotes = page_soup.select(".quote")
-    return [get_single_quote(quote) for quote in quotes]
+    return [parse_single_quote(quote) for quote in quotes]
 
 
-def has_next_page(page_soup):
+def has_next_page(page_soup: BeautifulSoup) -> bool:
     next_button = page_soup.select_one(".pager .next")
-    if next_button is not None:
-        return next_button
-    return None
+    return next_button is not None
 
 
-def get_all_quotes():
+def get_all_quotes() -> list[Quote]:
 
     all_quotes = []
-    start_page = 1
+    page_num = 1
 
     while True:
-        if start_page == 1:
+        if page_num == 1:
             url = BASE_URL
         else:
-            url = f"{BASE_URL}page/{start_page}/"
+            url = f"{BASE_URL}page/{page_num}/"
 
         response = requests.get(url)
         page_soup = BeautifulSoup(response.content, "html.parser")
@@ -55,24 +52,20 @@ def get_all_quotes():
         if not has_next_page(page_soup):
             break
 
-        start_page += 1
+        page_num += 1
 
     return all_quotes
 
 
-def get_file():
-    parse_quotes = get_all_quotes()
+def main(output_csv_path: str) -> None:
+    quotes = get_all_quotes()
 
-    with open("quotes.csv", "w", newline="", encoding="utf-8") as file:
-        writer = csv.writer(file)
+    with open(output_csv_path, "w", newline="", encoding="utf-8") as f:
+        writer = csv.writer(f)
         writer.writerow(["text", "author", "tags"])
 
-        for quote in parse_quotes:
-            writer.writerow([quote.text, quote.author, ", ".join(quote.tags)])
-
-
-def main(output_csv_path: str) -> None:
-    get_file()
+        for quote in quotes:
+            writer.writerow([quote.text, quote.author, str(quote.tags)])
 
 
 if __name__ == "__main__":
